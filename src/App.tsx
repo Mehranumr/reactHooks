b8a2de29da07a964fb30c91bc8c84185aea5021b
DESKTOP-M47BF6E\meer <mehran.khan@psw.gov.pk>
2021-07-15 13:48:11 +0500

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import UseStateHook from './components/UseStateSimple';
+import UseStateSimple from './components/UseStateSimple';
 import UseStateWithObject from './components/UseStateWithObject';
 
 function App() {
   return (
     <div className="App">
-      <UseStateHook />
+      <UseStateSimple />
       <UseStateWithObject />
     </div>
   );
